8b11e94857965fe8264b30d5180af66b01539fe9
ajg5226 <65829926+ajg5226@users.noreply.github.com>
2025-05-05 10:40:13 -0400

    Change default AUM to 30m

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from dotenv import load_dotenv
 load_dotenv()   # pulls in any .env overrides
 import os
 
-DEFAULT_AUM     = float(os.getenv("DEFAULT_AUM", "100000000"))
+DEFAULT_AUM     = float(os.getenv("DEFAULT_AUM", "30000000"))
 RISK_FREE_RATE  = float(os.getenv("RISK_FREE_RATE", "0.025"))
 REQUIRED_COLUMNS = ["Date", "GrossReturn"]
